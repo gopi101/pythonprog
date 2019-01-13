2581d3606030017d3d44e79e7796bd78c1c184da
itzmesatheesh <46571920+itzmesatheesh@users.noreply.github.com>
2019-01-13 19:39:09 +0530

    Update check whether a number is pallindrome or not.py

--- check whether a number is pallindrome or not.py
+++ check whether a number is pallindrome or not.py
@@ -1,4 +1,4 @@
-n=121
+n=1221
 temp=n
 rev=0
 while(n>0):
@@ -6,6 +6,6 @@ while(n>0):
     rev=rev*10+dig
     n=n//10
 if(temp==rev):
-    print("The number is a palindrome!")
+    print("yes")
 else:
-    print("The number isn't a palindrome!")
+    print("no")
